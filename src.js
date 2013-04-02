645c34d59df45fcbcd0083b5add92f99347aeae0
t0mg <tom@t0m.fr>
2013-04-02 18:17:14 +0300

    Create src.js

---
+++ src.js
@@ -0,0 +1,18 @@
+(function() {
+    var ifrm = document.createElement("iframe"),
+        rest = {},
+        cnt = 0,
+        debug = window.console?console.log:alert;
+    ifrm.setAttribute("style","display:none");
+    document.body.appendChild(ifrm);
+    var ifrw = (ifrm.contentWindow) ? ifrm.contentWindow : (ifrm.contentDocument.document);
+    for(var prop in window) {
+        if(!ifrw.hasOwnProperty(prop)) {
+           rest[prop] = window[prop];
+           cnt++;
+        }
+    }
+    ifrm.parentNode.removeChild(ifrm);
+    debug("Found " + cnt + " custom object(s)");
+    debug(rest);
+})();
